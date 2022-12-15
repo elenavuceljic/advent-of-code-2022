bd658bde0687558ecfc627477d71b14ebf85ecfb
Elena Vuceljic <elena.vuceljic@povio.com>
2022-12-15 18:25:20 +0100

    Day 03

---
+++ src/day03/Day03.kt
@@ -0,0 +1,41 @@
+package day03
+
+import java.io.File
+
+fun main() {
+
+    fun prioritiseItem(it: Char) = if (it >= 'a')
+        (it - 'a' + 1)
+    else
+        (it - 'A' + 27)
+
+    fun part1(input: String): Int {
+        val rucksacks = input.lines()
+        val duplicateItems = rucksacks.flatMap {
+            val (first, second) = it.chunked(it.length / 2) { it.toSet() }
+            val intersect = first.intersect(second)
+            println(intersect)
+            intersect
+        }
+        return duplicateItems.sumOf { prioritiseItem(it) }
+    }
+
+    fun part2(input: String): Int {
+        val rucksacks = input.lines().map { it.toSet() }
+        val compartments = rucksacks.chunked(3).flatMap {
+            it.reduce(Set<Char>::intersect)
+        }
+        return compartments.sumOf { prioritiseItem(it) }
+    }
+
+    val testInput = File("src/day03/Day03_test.txt").readText().trim()
+    val part1Solution = part1(testInput)
+    val part2Solution = part2(testInput)
+    println(part1Solution)
+    check(part1Solution == 157)
+    check(part2Solution == 70)
+
+    val input = File("src/day03/Day03.txt").readText().trim()
+    println(part1(input))
+    println(part2(input))
+}
